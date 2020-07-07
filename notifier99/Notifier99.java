0c45733f777ac1f5f2731bede67c676af8e646c1
lambdaprime <id.blackmesa@gmail.com>
2020-07-06 22:32:17 -0700

    Show amount of pause time left

--- notifier99/Notifier99.java
+++ notifier99/Notifier99.java
@@ -21,19 +21,69 @@
  *
  * @author lambdaprime <https://github.com/lambdaprime>
  */
-import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.layout.StackPane;
-import javafx.stage.Stage;
-import javafx.scene.control.Alert;
+import javafx.application.*;
+import javafx.scene.*;
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
+import javafx.stage.*;
+import javafx.scene.paint.*;
+import javafx.geometry.*;
+import javafx.beans.property.*;
 import javafx.scene.control.Alert.AlertType;
+import javafx.animation.*;
 
-import javafx.scene.media.Media;  
-import javafx.scene.media.MediaPlayer;  
-import javafx.scene.media.MediaView;
+import javafx.scene.media.*;  
 
 import javafx.util.Duration;
+
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+
+final String TITLE = "Notifier99";
+final int SCREEN_TIME_MINS = 20;
+final int PAUSE_TIME_MINS = 5;
+
+void showExpirationAlert(int seconds, Runnable onFinish) {
+    Alert alert = new Alert(AlertType.INFORMATION);
+    alert.setHeaderText("");
+    alert.setContentText("Unexpected error, application will be closed");
+    alert.setTitle(TITLE);
+
+    var timeSeconds = new SimpleLongProperty(seconds);
+    Label timerLabel = new Label();
+    timerLabel.textProperty().bind(timeSeconds.asString());
+    timerLabel.setTextFill(Color.RED);
+    timerLabel.setStyle("-fx-font-size: 4em;");
+
+    Instant end = Instant.now().plus(seconds, ChronoUnit.SECONDS); 
+    Timeline timeline = new Timeline();
+    timeline.setCycleCount(Timeline.INDEFINITE);
+    timeline.getKeyFrames().add(new KeyFrame(Duration.seconds(1), (event) -> {
+        var now = Instant.now();
+        var sec = java.time.Duration.between(now, end).toSeconds();
+        if (end.isBefore(now) || sec <= 0) {
+            timeSeconds.setValue(0);
+            timeline.stop();
+            onFinish.run();
+            return;
+        }
+        timeSeconds.setValue(sec - 1);
+    }));
+    timeline.playFromStart();
+
+    VBox vb = new VBox(20);
+    vb.setAlignment(Pos.CENTER);
+    vb.getChildren().addAll(timerLabel);
+    vb.setLayoutY(30);
+
+    alert.getDialogPane().setContent(vb);
+
+    alert.setOnCloseRequest(event -> {
+        onFinish.run();
+    });
+
+    alert.showAndWait();
+}
 
 class Notifier99 extends Application {
 
@@ -42,14 +92,16 @@ class Notifier99 extends Application {
         while (true) {
             var player = playSound();
             Alert alert = new Alert(AlertType.INFORMATION);
-            alert.setTitle("Notifier99");
+            alert.setTitle(TITLE);
             alert.setHeaderText("");
             alert.setContentText("Unexpected error, application will be closed");
             Stage alertStage = (Stage) alert.getDialogPane().getScene().getWindow();
             alertStage.setAlwaysOnTop(true);
             alert.showAndWait();
-            player.stop();
-            sleep(TimeUnit.MINUTES.toMillis(20));
+            showExpirationAlert((int)TimeUnit.MINUTES.toSeconds(PAUSE_TIME_MINS), () -> {
+                player.stop();
+            });
+            sleep(TimeUnit.MINUTES.toMillis(SCREEN_TIME_MINS));
         }
     }
 
@@ -68,7 +120,7 @@ class Notifier99 extends Application {
             }
         });
          
-        //by setting this property to true, the audio will be played   
+        // play the audio
         mediaPlayer.setAutoPlay(true);
 
         return mediaPlayer;
@@ -80,4 +132,3 @@ class Notifier99 extends Application {
 }
 
 Notifier99.main(null);
-//Notifier99.playSound();
