4efffe4720831470470cd3c04c310f30a02b9d32
lambdaprime <id.blackmesa@gmail.com>
2020-11-09 20:23:22 -0800

    Do not rely on history since if users never pressed any content links then pressing header link will sent them back to the previous page (and not to the top of the content)

--- generateTOC.js
+++ generateTOC.js
@@ -1,22 +1,33 @@
-/*
- * lambdaprime-scripts
- *
- * See README.org
- *
+/**
+ * Copyright 2020 lambdaprime
+ * 
+ * Email: id.blackmesa@gmail.com 
+ * Website: https://github.com/lambdaprime
  */
 
 var hElements = document.getElementsByTagName('*')
 var content = document.createElement('content')
+var prevPosition = 0;
 
 function newContentEntry(name, styleName) {
-    var div = document.createElement('div')
     var link = document.createElement('a')
-    //newDiv.className = 'content-class'
-    link.href = '#' + name
-    div.className = styleName
     link.innerHTML = name
+    link.setAttribute('href', '#' + name);
+    link.setAttribute('onclick', "savePosition()");
+    var div = document.createElement('div')
+    div.className = styleName
+    div.onclick = function () {  }
     div.appendChild(link)
     return div
+}
+
+function savePosition() {
+    prevPosition = document.body.scrollTop;
+    return false;
+}
+
+function goBack() {
+    document.body.scrollTop = prevPosition;
 }
 
 for (var i = 0; i < hElements.length; i++) {
@@ -24,39 +35,39 @@ for (var i = 0; i < hElements.length; i++) {
     if (hElements[i].nodeName == 'H1') {
         var link = document.createElement('a')
         link.name = hElements[i].innerHTML
-        link.href = "javascript:history.back()"
-        link.innerHTML = ">>>"
+        link.href = "javascript:goBack()"
+        link.innerHTML = hElements[i].innerHTML
         content.appendChild(newContentEntry(hElements[i].innerHTML, 'h1link'))
-        hElements[i].appendChild(link)
+        hElements[i].replaceChild(link, hElements[i].childNodes[0])
     } else if (hElements[i].nodeName == 'H2') {
         var link = document.createElement('a')
         link.name = hElements[i].innerHTML
-        link.href = "javascript:history.back()"
-        link.innerHTML = ">>>"
+        link.href = "javascript:goBack()"
+        link.innerHTML = hElements[i].innerHTML
         content.appendChild(newContentEntry(hElements[i].innerHTML, 'h2link'))
-        hElements[i].appendChild(link)
+        hElements[i].replaceChild(link, hElements[i].childNodes[0])
     } else if (hElements[i].nodeName == 'H3') {
         var link = document.createElement('a')
         link.name = hElements[i].innerHTML
-        link.href = "javascript:history.back()"
-        link.innerHTML = ">>>"
+        link.href = "javascript:goBack()"
+        link.innerHTML = hElements[i].innerHTML
         content.appendChild(newContentEntry(hElements[i].innerHTML, 'h3link'))
-        hElements[i].appendChild(link)
+        hElements[i].replaceChild(link, hElements[i].childNodes[0])
     } else if (hElements[i].nodeName == 'H4') {
         var link = document.createElement('a')
         link.name = hElements[i].innerHTML
-        link.href = "javascript:history.back()"
-        link.innerHTML = ">>>"
+        link.href = "javascript:goBack()"
+        link.innerHTML = hElements[i].innerHTML
         content.appendChild(newContentEntry(hElements[i].innerHTML, 'h4link'))
-        hElements[i].appendChild(link)
+        hElements[i].replaceChild(link, hElements[i].childNodes[0])
     } else if (hElements[i].nodeName == 'H5') {
         var link = document.createElement('a')
         link.name = hElements[i].innerHTML
-        link.href = "javascript:history.back()"
-        link.innerHTML = ">>>"
+        link.href = "javascript:goBack()"
+        link.innerHTML = hElements[i].innerHTML
         content.appendChild(newContentEntry(hElements[i].innerHTML, 'h5link'))
-        hElements[i].appendChild(link)
-    }    
+        hElements[i].replaceChild(link, hElements[i].childNodes[0])
+    }
+    
 }
-
 document.getElementsByTagName('content')[0].appendChild(content)
