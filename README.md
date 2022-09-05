d1c572c86c0c4cfc9e0e9c24c055e59d925786d9
lambdaprime <intid@protonmail.com>
2022-09-05 03:42:35 +0000

    Adding Windows 11 hibernation script

---
+++ README.md
@@ -0,0 +1,21 @@
+[generateTOC](generateTOC.pl) - generates table of content (TOC) in HTML files by parsing all header tags in it and inserting generated TOC inside of <content></content> tags of the original file.  
+The TOC is composed with hyperlinks by pressing which you will jump to required location in the document.
+Everything in between <exclude></exclude> tags are ignored.
+
+[generateTOC](generateTOC.js) - same as generateTOC.pl but builds TOC on the fly once you load the HTML file in the browser.
+
+[gitstat](gitstat.sh) - gathers the statistics about most changed files (java, xml, js) in git repository. The results are stored in files_*.txt files and includes the file name and number of times it was changed.
+
+[Notifier99](Notifier99.java) - shows particular message every 20 minutes.
+
+[mysql-ping](mysql-ping) - pings MySQL server indefinitely and prints the response time.
+
+[md-to-html](md-to-html.java) - converts Markdown to HTML.
+
+[gitall](gitall) - runs Git command recursively for all subfolders which are Git repositories.
+
+[recursive-run](recursive-run) - run commands recursively substituting all ${} placeholders for one value at a time.
+
+[winhiber](winhiber.bat) - helps to do autohibernate which is disabled in Windows 11.
+
+lambdaprime <intid@protonmail.com>
---
+++ winhiber.bat
@@ -0,0 +1,35 @@
+:: This batch script helps to do autohibernate which is disabled in Windows 11
+:: see https://answers.microsoft.com/en-us/windows/forum/all/windows-11-hibernate-enabled-cannot-auto-hibernate/4d2c4fde-c682-4ecd-a3c8-844dfa7c0b07
+:: Once this script installed, everytime when Windows goes to sleep it will
+:: automatically hibernate.
+::
+:: Setup:
+:: - Open Task Scheduler
+:: - Create Task
+::  - Name: winhiber
+::  - Run when user is logged on or not: Yes
+::  - Triggers -> New -> On an event
+::   - Log: System
+::   - Source: Kernel Power
+::   - Event ID: 506
+::  - Actions -> Start a program
+::   - Select this batch script
+::  - Settings
+::   - Do not start a new instance
+::
+:: Once hibernate is requested this tasks sleeps for 3min.
+:: It is needed to avoid infinite hibernation loop (when Windows
+:: wakes up it usually triggers the event again
+:: which triggers this script over and over). Adding sleep prevents Windows
+:: from starting yet another task right after hibernate. Unfortunatelly
+:: it has one side effect is that when you shutdown Windows then on
+:: next startup it will immediately hibernate. In this case you need to
+:: resume it back and it will start normally.
+::
+:: Copyright 2022 lambdaprime
+:: 
+:: Website: https://github.com/lambdaprime
+::
+
+shutdown /h
+timeout 180
